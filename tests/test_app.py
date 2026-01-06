c86da1bc3c040a203610d653b5dd595d88240247
m-abid2232 <muhammadabid2232@gamil.com>
2026-01-06 11:39:57 +0500

    all change now lets check it

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,8 @@
-import sys
-import os
-
-# Add project root to Python path so CI can find app.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app import app
-
 
 def test_home_page():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
+
 
